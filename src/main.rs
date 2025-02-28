ce6d142d96a1967ef89b89b625d21c1a6300626c
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2025-02-28 20:30:56 +0000

    Switch to embassy-rs, I2C blocking

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,67 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
 use rtt_target::{rprintln, rtt_init_print};
 
 use embassy_executor::Spawner;
 use embassy_rp::block::ImageDef;
 
+mod sensor_task;
+
+embassy_rp::bind_interrupts!(struct Irqs {
+    I2C0_IRQ => embassy_rp::i2c::InterruptHandler<embassy_rp::peripherals::I2C0>;
+});
+
+/// Entrypoint
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    // Initialise RTT logging
+
     rtt_init_print!();
     rprintln!("RTT logging initialised");
 
-    let _peripherals = embassy_rp::init(Default::default());
+    let peripherals = embassy_rp::init(Default::default());
+
+    let sda = peripherals.PIN_4;
+    let scl = peripherals.PIN_5;
+    let i2c_config = embassy_rp::i2c::Config::default();
+    let i2c_bus = embassy_rp::i2c::I2c::new_blocking(peripherals.I2C0, scl, sda, i2c_config);
+
+    embassy_time::Timer::after_millis(30).await; // SCD41 power-up delay
+    let mut scd41 = scd4x::Scd4x::new(i2c_bus, embassy_time::Delay);
+    scd41.wake_up();
+    scd41.reinit().unwrap();
+
+    match scd41.serial_number() {
+        Ok(serial) => rprintln!("[SCD41] Serial number: {}", serial),
+        Err(error) => rprintln!(
+            "[SCD41] Error: did not respond to get_serial_number: {:?}",
+            error
+        ),
+    }
+
+    scd41.start_periodic_measurement().unwrap();
+
+    loop {
+        embassy_time::Timer::after_secs(5).await;
+        match scd41.measurement() {
+            Ok(data) => {
+                rprintln!(
+                    "[SCD41] CO2: {} ppm, temperature: {} C, humidity: {} RH",
+                    data.co2,
+                    data.temperature,
+                    data.humidity
+                );
+            }
+            Err(error) => {
+                rprintln!(
+                    "[SCD41] Error: failed to retrieve measurement data: {:?}",
+                    error
+                );
+                break;
+            }
+        }
+    }
 
     loop {
         unsafe {
@@ -22,8 +70,9 @@ async fn main(_spawner: Spawner) {
     }
 }
 
+/// Panic handler
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
     rprintln!("Panicked! {}", info);
 
     loop {
@@ -33,17 +82,18 @@ fn panic(info: &PanicInfo) -> ! {
     }
 }
 
+/// Executable type header for the RP2350 bootloader
 #[link_section = ".start_block"]
 #[used]
 static IMAGE_DEF: ImageDef = ImageDef::secure_exe();
 
-// Program metadata for picotool
+/// Program metadata for picotool
 #[link_section = ".bi_entries"]
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_name!(c"Pico Environment Sensor"),
     embassy_rp::binary_info::rp_program_description!(
-        c"A CO2, temperature & humidity sensing application for the RPi Pico 2 W"
+        c"CO2, temperature & humidity sensing application for the RPi Pico 2 W"
     ),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
