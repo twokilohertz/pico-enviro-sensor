aee648bfa5e94e12a9dd8478fc45c4e8db0bb1f9
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2025-03-05 18:44:12 +0000

    Add framebuffer to prevent redraw flicker

--- src/display_task.rs
+++ src/display_task.rs
@@ -1,6 +1,7 @@
 // System
 use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_rp::{gpio::Output, spi::Config};
+use embedded_graphics_framebuf::FrameBuf;
 use rtt_target::rprintln;
 
 // Display
@@ -13,9 +14,11 @@ use ssd1351::{
 
 // Graphics
 use embedded_graphics::{
+    draw_target::DrawTarget,
     mono_font::{ascii::FONT_6X10, MonoTextStyle},
     pixelcolor::Rgb565,
-    prelude::{Point, WebColors},
+    prelude::{Point, RgbColor, Size, WebColors},
+    primitives::Rectangle,
     text::{Alignment, Text},
     Drawable,
 };
@@ -46,6 +49,10 @@ pub async fn display_output_task(
     display.reset(rst, &mut embassy_time::Delay).unwrap();
     display.init().unwrap();
 
+    // Framebuffer, prevents flickering on redraw
+    let buf = [Rgb565::BLACK; 128 * 128];
+    let mut framebuf = FrameBuf::new(buf, 128, 128);
+
     let co2_text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::CSS_DARK_GREEN);
     let temp_text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::CSS_ORANGE);
     let humidity_text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::CSS_AQUA);
@@ -55,7 +62,10 @@ pub async fn display_output_task(
     let mut humidity_text_buf = heapless::String::<16>::new();
 
     loop {
-        // Clear contents of buffers
+        // Clear the framebuffer
+        framebuf.clear(Rgb565::BLACK).unwrap();
+
+        // Clear contents of text buffers
         co2_text_buf.clear();
         temp_text_buf.clear();
         humidity_text_buf.clear();
@@ -66,9 +76,6 @@ pub async fn display_output_task(
         write!(&mut temp_text_buf, "Temp: {:.1} C", data.temperature).unwrap();
         write!(&mut humidity_text_buf, "RH: {:.1} %", data.humidity).unwrap();
 
-        // Clear the display
-        display.clear();
-
         // Draw the text to the screen
         Text::with_alignment(
             &co2_text_buf,
@@ -76,7 +83,7 @@ pub async fn display_output_task(
             co2_text_style,
             Alignment::Left,
         )
-        .draw(&mut display)
+        .draw(&mut framebuf)
         .unwrap();
 
         Text::with_alignment(
@@ -85,7 +92,7 @@ pub async fn display_output_task(
             temp_text_style,
             Alignment::Left,
         )
-        .draw(&mut display)
+        .draw(&mut framebuf)
         .unwrap();
 
         Text::with_alignment(
@@ -94,7 +101,11 @@ pub async fn display_output_task(
             humidity_text_style,
             Alignment::Left,
         )
-        .draw(&mut display)
+        .draw(&mut framebuf)
         .unwrap();
+
+        // Draw the entire framebuffer to the display
+        let area: Rectangle = Rectangle::new(Point::new(0, 0), Size::new(128, 128));
+        display.fill_contiguous(&area, framebuf.data).unwrap();
     }
 }
